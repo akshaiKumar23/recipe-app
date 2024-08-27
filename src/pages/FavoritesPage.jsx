a72ac8f3ee0f66d8f3fa1ff2b901457bdc70dc7b
Akshai Kumar <johanliebert1205@gmail.com>
2024-08-27 17:29:30 +0530

    implemented favorites logic

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,18 +1,21 @@
 import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utlis";
 
 
 const FavoritesPage = () => {
-    const fav = false;
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
     return (
         <div className="bg-[#fafbf9] flex-1 p-10 min-h-screen">
             <div className="max-w-screen-lg max-auto">
                 <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
-                {!fav && <div className="h-[80vh] flex flex-col items-center gap-4">
+                {favorites.length == 0 && <div className="h-[80vh] flex flex-col items-center gap-4">
                     <img src="/404.svg" className="h-3/4" alt="404" />
                 </div>}
-                {fav && <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <RecipeCard />
-                </div>}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {favorites.map((favorite, index) => (
+                        <RecipeCard key={index} recipe={favorite} {...getRandomColor()} />
+                    ))}
+                </div>
             </div>
         </div>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,44 @@
 import { Search } from "lucide-react"
 import RecipeCard from "../components/RecipeCard"
+import { useEffect, useState } from "react"
+import { getRandomColor } from './../lib/utlis';
+
 
 const HomePage = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    const fetchRecipes = async (searchQuery) => {
+        setLoading(true);
+        setRecipes([]);
+
+        try {
+            const res = await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${import.meta.env.VITE_APP_ID}&app_key=${import.meta.env.VITE_APP_KEY}&q=${searchQuery}&type=public`)
+            const data = await res.json();
+            console.log(data);
+            setRecipes(data.hits)
+
+        } catch (error) {
+            console.log(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+
+        fetchRecipes("chicken");
+    }, [])
+    const handleSearchRecipe = (e) => {
+        e.preventDefault();
+        const searchQuery = e.target[0].value;
+        fetchRecipes(searchQuery);
+    }
     return (
         <div className="flex-1 bg-[#faf9fb] p-10">
             <div className="max-w-screen-lg mx-auto">
-                <form>
+                <form onSubmit={handleSearchRecipe}>
                     <label className="input shadow-md flex items-center gap-2">
                         <Search size={24} />
                         <input type="text" className="grow text-sm md:text-md" placeholder="What do you want to eat today?" />
@@ -17,11 +50,20 @@ const HomePage = () => {
                 </p>
                 <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">Popular Choice</p>
                 <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
+                    {!loading && recipes.map(({ recipe }, index) => (
+                        <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+                    ))}
+                    {loading &&
+                        [...Array(9)].map((_, index) => (
+                            <div key={index} className='flex flex-col gap-4 w-full'>
+                                <div className='skeleton h-32 w-full'></div>
+                                <div className='flex justify-between'>
+                                    <div className='skeleton h-4 w-28'></div>
+                                    <div className='skeleton h-4 w-24'></div>
+                                </div>
+                                <div className='skeleton h-4 w-1/2'></div>
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
